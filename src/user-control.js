637328a80a5539437f9abc6a13deb6a009210113
Maureen Gloria <maureenmgloria@gmail.com>
2020-09-26 23:07:19 -0500

    add test files

---
+++ src/user-control.js
@@ -0,0 +1,61 @@
+
+document.addEventListener('keydown', event => {
+
+    console.log(event.key);
+    switch (event.key) {
+        case 'p':
+            toggleColor();
+
+            break;
+
+        case 'o':
+            //document.getElementById('chi2').setAttribute('visible', 'false')
+            //console.log(document.getElementById('chi'))
+            break;
+
+        default:
+            break;
+    }
+})
+
+AFRAME.registerComponent('poopoo',
+    {
+        init: function () {
+            this.el.addEventListener('mouseover', function(e){
+                console.log("he!");
+
+            })
+        }
+    }
+)
+
+AFRAME.registerComponent('change-color-on-hover', {
+
+
+    init: function () {
+      var data = this.data;
+      var el = this.el;  // <a-box>
+
+      el.addEventListener('mousedown', function () {
+        console.log('clicked')
+      });
+    }
+  });
+
+function toggleColor() {
+    console.log(document.getElementById('scene'))
+
+    console.log(document.getElementById('scene').getAttribute('renderer'))
+
+    var value = !document.getElementById('scene').getAttribute('renderer').colorManagement
+
+    document.getElementById('scene').setAttribute('renderer', `colorManagement: ${value}; antialias: false`)
+
+    var body = document.getElementById('aframe-wrapper')
+
+    var content = body.innerHTML;
+
+    body.innerHTML = content
+
+    // scene.setAttribute('renderer', 'colorManagement', 'true')
+}
